bfb759360c8908671e8c6da0b1981b4103afd16e
zackbrand <zackbrand@gmail.com>
2017-02-20 15:55:22 -0500

    Moved favicon so inliner can handle it instead of JavaScript

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,5 @@
 (
   npm run inline
   firebase deploy
+  start chrome "https://firebrand-2a88e.firebaseapp.com/"
 )
--- src/js/master.js
+++ src/js/master.js
@@ -1,7 +1,1 @@
 const sample = "sample";
-
-const docHead = document.getElementsByTagName('head')[0];       
-const newLink = document.createElement('link');
-newLink.rel = 'shortcut icon';
-newLink.href = 'data:image/png;base64,'+favicon;
-docHead.appendChild(newLink);
