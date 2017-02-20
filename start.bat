0a90e0278200a3abdac2bb17a4d825a0badbf8e7
zackbrand <zackbrand@gmail.com>
2017-02-19 22:15:14 -0500

    start.bat will now update node dependencies before starting services

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
-@echo on
-  
-	IF NOT EXIST "node_modules\" (npm install)
-    start chrome "http://localhost:7000"
-    npm run start
+@echo off
+(
+  npm install
+  start chrome "http://localhost:7000"
+  npm run start
+)
