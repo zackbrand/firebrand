e38b2390bc46dc420cfada4ea4b494471dc376b5
zackbrand <zackbrand@gmail.com>
2017-02-20 19:54:31 -0500

    Added cache versioning to service worker

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,6 +1,10 @@
+// Use unique cache name to allow for graceful updating
+const cacheVersion = "v1";
+
+// Install default cached files
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open(cacheVersion).then(function(cache) {
       return cache.addAll([
         '/'
       ]);
@@ -8,11 +12,26 @@ this.addEventListener('install', function(event) {
   );
 });
 
+// Cleanup old cache after new SW is activated
+this.addEventListener('activate', function(event) {
+  const cacheWhitelist = [cacheVersion];
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Intercept asset requests, check cache first, then check network
 this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
+        return caches.open(cacheVersion).then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
         });  
